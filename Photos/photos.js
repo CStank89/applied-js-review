29c2ff298c0d3bbb981fd2e306e2b17d057149c7
Buddy Agyin <agyin3@gmail.com>
2020-09-05 14:22:46 -0600

    initial commit

---
+++ Photos/photos.js
@@ -0,0 +1,17 @@
+// Make a GET Request to the following url: https://picsum.photos/v2/list
+// Make sure to examine the response first to see what the data looks like
+
+// Create a function that will take in an object create the following:
+/* 
+    <div class='photo-card'>
+        <div class='img-wrapper'>
+            <img src={download url} />
+        </div>
+        <p> Author: {autho's name}</p>
+        <p>Original Height: {img height}</p>
+        <p>Original Width: {img width}</p>
+    </div>
+*/
+
+// Use your function to create a crad for each of the images and append it
+// to the div with the class name 'photo-container'
---
+++ SpaceX/spacex.js
@@ -0,0 +1,21 @@
+// Make a GET Request to the following url: https://api.spacexdata.com/v4/launches/latest
+// Make sure to examine the response first to see what the data looks like
+
+// Create a function that will take in an object create the following:
+/* 
+    <div class='space-x-card'>
+        <div class="img-wrapper">
+            <img src={small patch} />
+        </div>
+        <h3>Details</h3>
+        <h4>Name: {name of ship}</h4>
+        <p>{details about ship}</p>
+        <h3>Links</h3>
+        <a href={reddit campaign}>Reddit Campaign</a>
+        <a href={webcast}>Youtube Webcast</a>
+        <a href={wikipedia}>Wikipedia</a>
+    </div>
+*/
+
+// Use your function to create a crad for and append it
+// to the div with the class name 'space-x'
